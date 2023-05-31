42fa7959e50867edade9ff6be9450e6bd13e4fff
Prop4et <francostanco97@gmail.com>
2023-05-31 13:23:47 +0200

    sf10 dr2

--- src/mac/region/RegionEU868.h
+++ src/mac/region/RegionEU868.h
@@ -62,12 +62,12 @@ extern "C"
 /*!
  * Minimal datarate that can be used by the node
  */
-#define EU868_TX_MIN_DATARATE                       DR_0
+#define EU868_TX_MIN_DATARATE                       DR_2
 
 /*!
  * Maximal datarate that can be used by the node
  */
-#define EU868_TX_MAX_DATARATE                       DR_0
+#define EU868_TX_MAX_DATARATE                       DR_2
 
 /*!
  * Minimal datarate that can be used by the node
@@ -82,7 +82,7 @@ extern "C"
 /*!
  * Default datarate used by the node
  */
-#define EU868_DEFAULT_DATARATE                      DR_0
+#define EU868_DEFAULT_DATARATE                      DR_2
 
 /*!
  * Minimal Rx1 receive datarate offset
